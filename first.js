99e5b226c664af2f31e375f79a85981c1713dcd8
Samirmansuri2006 <samirmansuri1207@gmail.com>
2025-06-15 17:07:29 +0530

    Create first.js

---
+++ first.js
@@ -0,0 +1,62 @@
+console.log("fronter: hellow samir");
+console.log("fronter: how are you");
+console.log("yaa i am fine what about you")
+console.log("fronter: what about you studying");
+console.log(" i studying about computer engineering");
+fullname="samir Mansuri";
+console.log(fullname);
+age=24;
+console.log(age);
+price=1000.50;
+console.log(price);
+a=null;
+console.log(a);
+b=undefined;
+console.log(b);
+isFollow=true;
+console.log(isFollow);
+var c=22;
+var c=23;
+console.log(c);
+let d=33;
+let e=55;
+console.log(d);
+console.log(e);
+const f=44;
+const y=66;
+console.log(f);
+console.log(y);
+{
+    let a=22;
+    console.log(a);
+}
+{
+    let a=22;
+    console.log(a);
+}
+{
+    let a=55;
+    console.log(a);
+}
+const teacher={
+    fullName:"samir",
+    age: 22,
+    isFollow:true,
+    price:2000.0,
+}
+a=33+44;
+console.log(a);
+a=33-22;
+console.log(a);
+a=10*2;
+console.log(a);
+a=10/2;
+console.log(a);
+for(let i=0;i<10;i++){
+    console.log(i);
+}
+let s=2
+while(s<=10){
+    console.log(s);
+    s+=2;
+}
